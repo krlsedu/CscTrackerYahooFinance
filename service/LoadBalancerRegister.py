4b1e64f3f035c5af83f125d04d05e715edada2d1
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2023-12-07 19:43:27 -0300

    The given commit message does not correspond to the provided diff. Based on the diff, a suitable commit message could be:
    Disable LoadBalancerRegister functionality
    
    In this commit, the entire functionality for registering a load balancer has been commented out. This change affects the 'register' method in the 'LoadBalancerRegister.py' file and temporarily halts the registration of any services to the load balancer.

--- service/LoadBalancerRegister.py
+++ service/LoadBalancerRegister.py
@@ -21,16 +21,15 @@ class LoadBalancerRegister(Interceptor):
     def register(self, service_name, host_name=None, port=None):
         if host_name is None:
             host_name = socket.gethostname()
-        if port is None:
-            port = 5000
-        pod = {'service': service_name, 'host': host_name, 'port': port}
-        headers = {'Content-Type': 'application/json'}
-        try:
-            response = requests.post('http://balancer:8080/pod', json=pod, headers=headers)
-            if response.status_code < 200 or response.status_code > 299:
-                print(pod, response.status_code)
-                print(f'Error sending metrics: {response.text}')
-            pass
-        except Exception as e:
-            print(e)
-            pass
+        # if port is None:
+        #     port = 5000
+        # pod = {'service': service_name, 'host': host_name, 'port': port}
+        # headers = {'Content-Type': 'application/json'}
+        # try:
+        #     response = requests.post('http://balancer:8080/pod', json=pod, headers=headers)
+        #     if response.status_code < 200 or response.status_code > 299:
+        #         print(pod, response.status_code)
+        #         print(f'Error sending metrics: {response.text}')
+        #     pass
+        # except Exception as e:
+        #     pass
